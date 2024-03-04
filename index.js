af7ade739f79a1acb6eeec0bb47bad42ad67db8a
steedude <85280987+steedude@users.noreply.github.com>
2024-03-04 14:08:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+
+app.get('/', (req, res) => {
+  res.send('hello, world！')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Running cors-proxy on ${PORT}`)
+})
