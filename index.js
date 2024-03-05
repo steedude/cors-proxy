42043befc1e4422aa4a2c8b36e038a07ad9be3a0
steedude <85280987+steedude@users.noreply.github.com>
2024-03-05 18:51:27 +0800

    test: ci

--- index.js
+++ index.js
@@ -1,51 +1,38 @@
-// const http = require('http')
-// const httpProxy = require('http-proxy')
-// const url = require('url')
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
-const express = require('express')
-const app = express()
-
+const http = require('http')
+const httpProxy = require('http-proxy')
+const url = require('url')
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
-app.get('/', (req, res) => {
-  res.send('Hello, World！')
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true)
+  const userUrl = parsedUrl.query.url
+
+  if (!userUrl) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Hello, World!')
+    return
+  }
+
+  console.log('User URL:', userUrl)
+
+  const targetHost = new URL(userUrl).hostname
+
+  const proxy = httpProxy.createProxyServer({})
+
+  proxy.on('error', (err, req, res) => {
+    console.error('Proxy error:', err)
+    res.writeHead(500, { 'Content-Type': 'text/plain' })
+    res.end('Proxy error')
+  })
+
+  proxy.web(req, res, {
+    target: userUrl,
+    changeOrigin: true,
+    targetHost: targetHost,
+  })
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Running cors-proxy on ${PORT} port.`)
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
-
-// const server = http.createServer((req, res) => {
-//   const parsedUrl = url.parse(req.url, true)
-//   const userUrl = parsedUrl.query.url
-
-//   if (!userUrl) {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' })
-//     res.end('Hello, World!')
-//     return
-//   }
-
-//   console.log('User URL:', userUrl)
-
-//   const targetHost = new URL(userUrl).hostname
-
-//   const proxy = httpProxy.createProxyServer({})
-
-//   proxy.on('error', (err, req, res) => {
-//     console.error('Proxy error:', err)
-//     res.writeHead(500, { 'Content-Type': 'text/plain' })
-//     res.end('Proxy error')
-//   })
-
-//   proxy.web(req, res, {
-//     target: userUrl,
-//     changeOrigin: true,
-//     targetHost: targetHost,
-//   })
-// })
-
-// const PORT = process.env.PORT || 3000
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`)
-// })
